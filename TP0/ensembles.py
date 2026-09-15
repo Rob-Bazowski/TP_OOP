a764b0a580f6090a3db0485e0c37599aae8d39bd
Marin <marin@cataclop.com>
2026-09-15 15:26:06 +0200

    Exercice 4 - Ensembles | Question 2 - Ajout/Suppression d'un élément d'un ensemble

--- TP0/ensembles.py
+++ TP0/ensembles.py
@@ -18,3 +18,26 @@ exploration_seule = robots_exploration_seulement(robots_exploration, robots_tran
 assert double_mission == {"R5", "R7"}
 assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
 assert exploration_seule == {"R2"}
+
+# Question 2
+
+def ajouter_robot_mission(ensemble, robot_a_ajouter):
+    nouveau_ensemble = set()
+    for robot in ensemble:
+        nouveau_ensemble.add(robot)
+    nouveau_ensemble.add(robot_a_ajouter)
+    return nouveau_ensemble
+
+def retirer_robot_mission(ensemble, robot_a_retirer):
+    nouveau_ensemble = set()
+    for robot in ensemble:
+        if robot != robot_a_retirer:
+            nouveau_ensemble.add(robot)
+    return nouveau_ensemble
+
+ajout = ajouter_robot_mission(robots_exploration, "R8")
+retrait = retirer_robot_mission(robots_transport, "R9")
+assert ajout == {"R2", "R5", "R7", "R8"}
+assert retrait == {"R3", "R5", "R7"}
+# L’ensemble d’origine ne doit pas avoir été modifié
+assert robots_transport == {"R5", "R9", "R7", "R3"}
