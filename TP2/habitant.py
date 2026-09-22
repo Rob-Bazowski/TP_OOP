d6302c486e8dd848c2015f98d3ef3cf5a3ca855c
Marin <marin@cataclop.com>
2026-09-22 14:31:24 +0200

    Exercice 4 - Encapsulation | Question 1 - Attributs privés

--- TP2/habitant.py
+++ TP2/habitant.py
@@ -1,33 +1,50 @@
-"""Exercice 3 - De la fonction à la classe Habitant"""
-
-# Question 1
+"""Exercice 3 - De la fonction à la classe Habitant / Exercice 4 - Encapsulation"""
 
 class Habitant:
     """Classe Habitant contenant le nom, l'âge, l'adresse et les animaux d'un habitant"""
 
     def __init__(self, nom, age, adresse, animaux = None):
-        self.nom = nom
-        self.age = age
-        self.adresse = adresse
+        self.__nom = nom
+        self.__age = age
+        self.__adresse = adresse
         if animaux is not None:
-            self.animaux = animaux
+            self.__animaux = animaux
         else:
-            self.animaux = {}
+            self.__animaux = {}
 
-# Question 2
+    def get_nom(self):
+        return self.__nom
+
+    def get_age(self):
+        return self.__age
+
+    def get_adresse(self):
+        return self.__adresse
+    
+    def get_animaux(self):
+        return self.__animaux
+
+    def set_age(self, age):
+        self.__age = age
+
+    def set_adresse(self, adresse):
+        self.__adresse = adresse
+
+    def set_animaux(self, animaux):
+        self.__animaux = animaux
 
     def affichage_adresse(self):
         """Affiche l'adresse d'un habitant"""
-        print(f"{self.nom} habite a Rue {self.adresse}")
+        print(f"{self.__nom} habite a Rue {self.__adresse}")
 
     def compte_animal(self, animal):
         """Compte le nombre le compte d'un certain animal qu'a un habitant"""
-        if animal in self.animaux.keys():
-            return self.animaux[animal]
+        if animal in self.__animaux.keys():
+            return self.__animaux[animal]
         return 0
 
 h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
-assert h1.nom == "Aldric"
+assert h1.get_nom() == "Aldric"
 assert h1.compte_animal("vaches") == 3
 assert h1.compte_animal("moutons") == 0
 h1.affichage_adresse() # affiche "Aldric habite a Rue A"
