d59562ae3c178d6db93ee84b8a77d10101f2ff2f
Marin <marin@cataclop.com>
2026-09-22 14:20:01 +0200

    Exercice 3 - De la fonction à la classe Habitant | Question 2 - Méthodes

--- TP2/habitant.py
+++ TP2/habitant.py
@@ -13,3 +13,21 @@ class Habitant:
             self.animaux = animaux
         else:
             self.animaux = {}
+
+# Question 2
+
+    def affichage_adresse(self):
+        """Affiche l'adresse d'un habitant"""
+        print(f"{self.nom} habite a Rue {self.adresse}")
+
+    def compte_animal(self, animal):
+        """Compte le nombre le compte d'un certain animal qu'a un habitant"""
+        if animal in self.animaux.keys():
+            return self.animaux[animal]
+        return 0
+
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+assert h1.nom == "Aldric"
+assert h1.compte_animal("vaches") == 3
+assert h1.compte_animal("moutons") == 0
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"
